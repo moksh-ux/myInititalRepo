d1176bd79c7ce75215a75a33aa21f4a28df44bfc
moksh-ux <modhtyagi@gmail.com>
2025-08-05 17:16:47 +0530

    Initital Commit to Push to a new repo

---
+++ calc.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() {
+    double num1, num2, result;
+    char op;
+
+    std::cout << "Simple Calculator" << std::endl;
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            std::cout << "Result: " << result << std::endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "Result: " << result << std::endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "Result: " << result << std::endl;
+            break;
+        case '/':
+            if(num2 != 0) {
+                result = num1 / num2;
+                std::cout << "Result: " << result << std::endl;
+            } else {
+                std::cout << "Error: Division by zero!" << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Invalid operator." << std::endl;
+    }
+}
